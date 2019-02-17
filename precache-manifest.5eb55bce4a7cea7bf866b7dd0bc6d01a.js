ee8c7505d44e3ccd59dc3ab78ef7db63bfbcbf0e
Arpit Kapadia <kapadiaarpit@gmail.com>
2019-02-18 02:13:28 +0530

    Updates

---
+++ precache-manifest.5eb55bce4a7cea7bf866b7dd0bc6d01a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RevenueDashboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "95706a08b2219a7b487c",
+    "url": "/RevenueDashboard/static/js/runtime~main.95706a08.js"
+  },
+  {
+    "revision": "085da83a60073f28c576",
+    "url": "/RevenueDashboard/static/js/main.085da83a.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/RevenueDashboard/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "085da83a60073f28c576",
+    "url": "/RevenueDashboard/static/css/main.e5db8fba.chunk.css"
+  },
+  {
+    "revision": "583d67b9d9d57b574f1db135307bdc7c",
+    "url": "/RevenueDashboard/index.html"
+  }
+];
